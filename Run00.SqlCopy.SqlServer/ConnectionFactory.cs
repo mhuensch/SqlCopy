2a47dfdabac727293513fd5f8b80bad2e327c02b
Michael Huensch <support@run00.com>
2013-08-29 10:26:35 -0500

    Fixing broken build

---
+++ Run00.SqlCopy.SqlServer/ConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using Run00.SqlCopy;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Run00.SqlCopySqlServer
+{
+	public class ConnectionFactory : IConnectionFactory
+	{
+		IDbConnection IConnectionFactory.Create(DatabaseInfo info)
+		{
+			return new SqlConnection(info.ConnectionString);
+		}
+	}
+}
---
+++ Run00.SqlCopy.SqlServer/TableBulkCopy.cs
@@ -0,0 +1,24 @@
+﻿using Run00.SqlCopy;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Run00.SqlCopySqlServer
+{
+	public class TableBulkCopy : ITableBulkCopy
+	{
+		void ITableBulkCopy.Copy(IDbConnection connection, string tableName, IDataReader reader)
+		{
+			var sqlConnection = connection as SqlConnection;
+			if (sqlConnection == null)
+				throw new InvalidOperationException();
+
+			var copy = new SqlBulkCopy(sqlConnection);
+			copy.DestinationTableName = tableName;
+			copy.WriteToServer(reader);
+		}
+	}
+}
